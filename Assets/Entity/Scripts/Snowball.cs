823798092718882b0b8446c375addb5808fa0cb8
pigsteel <piggylogan1@gmail.com>
2024-06-06 12:33:46 -0400

    Snowball lifetime

--- Assets/Entity/Scripts/Snowball.cs
+++ Assets/Entity/Scripts/Snowball.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Snowball : MonoBehaviour {
     private Rigidbody rb;
+    private float lifetime = 0.0f;
+
 
     public void ApplySpawn(Vector3 pos, Vector3 velocity) {
         rb = GetComponent<Rigidbody>();
@@ -11,5 +14,11 @@ public class Snowball : MonoBehaviour {
         rb.position = pos;
         transform.position = pos;
         rb.interpolation = RigidbodyInterpolation.Interpolate;
+    }
+
+    public void FixedUpdate() {
+        lifetime += Time.fixedDeltaTime;
+        if(lifetime > 100.0f)
+            Destroy(this);
     }
 }
