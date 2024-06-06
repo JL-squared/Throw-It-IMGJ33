0f803cc9c3fb55071fe376dab2eb17c1f03b9bd9
pigsteel <piggylogan1@gmail.com>
2024-06-06 10:26:46 -0400

    Crafting changes

--- Assets/Items/Scripts/CraftingRecipe.cs
+++ Assets/Items/Scripts/CraftingRecipe.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CraftingRecipe : ScriptableObject {
     public List<Item> requirements;
     public Item output;
     public int craftingTime;
+
+    public bool CheckForRequirements(List<Item> items) {
+        int requirementsLeft = items.Count;
+
+        foreach(Item item in items) {
+            for(int i = requirementsLeft; i >= 0; i--) { // Count down instead of up
+                var req = requirements[i];
+                if(item.data.id == req.data.id && item.Count == req.Count) {
+                    requirementsLeft--;
+                }
+            }
+        }
+
+        return requirementsLeft == 0;
+    }
 }
