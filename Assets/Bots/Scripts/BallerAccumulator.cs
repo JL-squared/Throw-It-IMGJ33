7d664f12a4dc9cd209f7d2045076c048bc7c1b0f
Jedjoud10 <jribijed6@gmail.com>
2024-06-09 00:59:50 -0400

    Added lobotomy sparks and improved baller ball

--- Assets/Bots/Scripts/BallerAccumulator.cs
+++ Assets/Bots/Scripts/BallerAccumulator.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class BallerAccumulator : MonoBehaviour {
     public Transform ball;
     public float increaseFactor;
-    public float rotationSpeed;
     public float startingRadius;
     private float volume;
     private float radius;
     private CharacterController cc;
     private EntityMovement movement;
+    private Quaternion rotation = Quaternion.identity;
+    private Quaternion angularVelocity = Quaternion.identity;
 
     public void Start() {
         volume = (4f / 3f) * Mathf.PI * Mathf.Pow(startingRadius, 3);
@@ -22,14 +23,20 @@ public class BallerAccumulator : MonoBehaviour {
     void UpdateBallParams() {
         cc.Move(Vector3.up * radius * Time.deltaTime);
         radius = Mathf.Sqrt(volume / 4 * Mathf.PI);
-        ball.transform.localScale = Vector3.one * radius * 2f;
-        ball.transform.localPosition = -Vector3.up * (radius + 0.5f);
         cc.center = Vector3.up * (-radius);
         cc.height = 1f + 2 * radius;
-        ball.Rotate(Vector3.right, rotationSpeed * Time.deltaTime / radius, Space.Self);
+        ball.localScale = Vector3.one * radius * 2f;
+        ball.localPosition = -Vector3.up * (radius + 0.5f);
+
+        Vector2 mov2d = new Vector2(movement.wishMovement.x, movement.wishMovement.z);
+        float lateralSpeed = mov2d.magnitude;
+        float rotationSpeed = Mathf.Rad2Deg * lateralSpeed / radius;
+        Vector3 dir = transform.TransformDirection(Vector3.right);
+        angularVelocity = Quaternion.AngleAxis(rotationSpeed * Time.deltaTime, dir);
     }
 
     public void Update() {
+
         if (cc.isGrounded) {
             Vector2 mov2d = new Vector2(movement.wishMovement.x, movement.wishMovement.z);
             volume += increaseFactor * Time.deltaTime * mov2d.magnitude;
@@ -37,6 +44,14 @@ public class BallerAccumulator : MonoBehaviour {
             if (mov2d.magnitude > 0.01f) {
                 UpdateBallParams();
             }
+        } else {
+            angularVelocity = Quaternion.identity;
         }
+
+        rotation = angularVelocity.normalized * rotation;
+        
+        // I tried setting global rotation to default but that didn't work idk why
+        ball.localRotation = Quaternion.Inverse(transform.rotation);
+        ball.rotation = rotation;
     }
 }
