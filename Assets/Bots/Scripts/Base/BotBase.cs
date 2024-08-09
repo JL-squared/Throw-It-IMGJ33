3f20846c04dcd6dda0dd1da4007338787edbef9c
Jedjoud10 <jribijed6@gmail.com>
2024-08-09 18:58:46 -0400

    Tested android build. Works fine!!

--- Assets/Bots/Scripts/Base/BotBase.cs
+++ Assets/Bots/Scripts/Base/BotBase.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Xml.Schema;
-using UnityEditor.UI;
 using UnityEngine;
 
 public class BotBase : MonoBehaviour {
--- Assets/Scenes/Scripts/MarketManager.cs
+++ Assets/Scenes/Scripts/MarketManager.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using UnityEditor.AddressableAssets.Build;
 using UnityEngine;
 
 public class ItemStock {
